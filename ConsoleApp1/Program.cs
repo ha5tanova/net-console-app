759e6c3c17e2570dacf4c22a22c9d2369c02f038
Aleksandra Khaptanova <thehny8@gmail.com>
2023-09-11 10:18:18 +0800

    Суммирование массива чисел

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int[] mas = { 1, 2, 3,4, 5, 6,7,8,9 ,10 };
+            for(int i = 0; i< mas.Length; i++)
+            {
+                Console.Write(mas[i]+" ");
+            }
+            int sum = 0;
+            sum = mas.Sum();
+            Console.WriteLine();
+            Console.WriteLine("Суммма чисел:" + sum);
+        }
+    }
+}
