a5c50a49320904b754f5909dc2e20b931d7c40d6
Aleksandra Khaptanova <thehny8@gmail.com>
2023-09-18 09:47:45 +0800

    Добавлен тест на проверку 0(четный или  нечетный)

---
+++ TestProject1/TestIsEvenNumber0.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class TestIsEvenNumber2
+    {
+        [Fact]
+        public void TestIsEvenNumber0()
+        {
+
+            CheckIsEvenNumber checkIsEvenNumber0 = new CheckIsEvenNumber();
+            int num = 0;
+            bool IsEven_Null = checkIsEvenNumber0.IsEven(num);
+            Assert.True(IsEven_Null);
+;
+        }
+
+        public bool IsEven(int num)
+        {
+
+            return num % 2 == 0;
+        }
+    }
+}
