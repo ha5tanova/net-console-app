60f0877c4b4f0a53777381f8bf5ce63668c0a0cf
Aleksandra Khaptanova <thehny8@gmail.com>
2023-09-18 10:23:21 +0800

    Добавлен тест на проверку пустая ли строка с помощью Assert.Empty

---
+++ TestProject1/CheckForInput.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class CheckForInput
+    {
+        [Fact]
+        public void CheckInput() {
+
+            string num = "";
+            
+            Assert.Empty(num);
+
+            string num1 = "123";
+
+            Assert.NotEmpty(num1);
+
+
+        }
+
+        
+    }
+}
